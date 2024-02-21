f14ed87d61ea1e66c8ca9fdd29edb6d4153cd5e0
LadanOraki <ladan.oraki@gmail.com>
2024-02-21 14:13:03 +0330

    ftgt

--- Identity.Data/Context/IdentityContext.cs
+++ Identity.Data/Context/IdentityContext.cs
@@ -1,7 +1,22 @@
-﻿namespace Identity.Data.Context
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Data.Context
 {
-    public class IdentityContext
+    public class IdentityContext : IdentityDbContext
     {
+        public IdentityContext()
+        {
+                
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+
 
     }
 }
